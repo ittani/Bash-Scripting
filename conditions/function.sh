d261d137794b6d61e8bc6ab781f16ba41f5fb045
Ittani Maemo <ittani@andilesolutions.com>
2025-01-31 14:15:03 +0200

    Updated new Script

--- conditions/function.sh
+++ conditions/function.sh
@@ -3,14 +3,71 @@
 # Learning bash condition handling
 # Taking input from the user
 
-echo "Enter the name of your continent"
-read name
+echo "************************************"
+echo "RoseBank Solution ltd."
+echo "************************************"
+
+echo "Enter current cost of the company per hour e.g your rate per hour"
+read cost
 
 
-if [ $name = "Africa"  ]
+if [ $cost -le 400  ]
 then 
-    echo "The continent is Africa!"
+    echo "Great!"
+    echo  "how much would you like to be compesanted!"
+     read comp
+        if [ $comp -gt 400 ]
+         then 
+                 echo "This is beyond our budget."
+                 echo "Our estimate budget is 400 per hour. Is this something you might consider?? "
+                 read answer
+
+                        if [ $answer = "yes" ]
+                        then
+                            echo "Thanks for the feedback. Please the following: "
+                            read -p "1. name: " name
+                            read -p "2. Email: " email
+                            read -p "3. phone: "  phone
+
+                            echo "Hi,  $name thank you for choosing us! "
+                            echo "Please, view the email we sent to your $email"
+                        else
+                                echo "Thank you for your patience and interest, unfortunately we won't be able to accomodate you."
+
+                        fi
+        else
+                echo "Thanks for the feedback. Please the following: "
+                read -p "1. name: " name
+                read -p "2. Email: " email
+                read -p "3. phone: "  phone
+
+                 echo "Hi,  $name thank you for choosing us! "
+                 echo "Please, view the email we sent to your $email"
+        fi
+
+elif [ $cost -gt 400  ]
+then 
+    echo "This is beyond our budget."
+    echo "Our estimate budget is 400 per hour. Is this something you might consider?? "
+                 read answer
+
+                        if [ $answer = "yes" ]
+                        then
+                            echo "Thanks for the feedback. Please the following: "
+                            read -p "1. name: " name
+                            read -p "2. Email: " email
+                            read -p "3. phone: "  phone
+
+                            echo "Hi,  $name thank you for choosing us! "
+                            echo "Please, view the email we sent to your $email"
+                        else
+                                echo "Thank you for your patience and interest, unfortunately we won't be able to accomodate you."
+
+                        fi
+
+else
+    echo "Invalid input"
 fi
 
-# we are comparing strings
+
 
